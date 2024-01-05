12050c4655b0538415f59c3f1d757e167857bd11
Roger Herikstad <roger.herikstad@gmail.com>
2024-01-05 10:02:45 +0800

    Add functions for generating spikes

--- src/SpiketrainGeneration.jl
+++ src/SpiketrainGeneration.jl
@@ -31,4 +31,35 @@ get_threshold(q::ExponentialProcess) = -log(rand())
 do_step(q::ExponentialProcess, λ, dt) = λ*dt
 
 
+"""
+    generate_spikes(spike_process::SpikeProcess, λ::Vector{Float64};tmin=0.0, tmax=Inf, dt=0.1)
+
+Generate a spike train using the supplied `spike_process` and rate vector `λ`
+"""
+function generate_spikes(spike_process::SpikeProcess, λ::Vector{Float64};tmin=0.0, tmax=Inf, dt=0.1)
+    _sp = Float64[]
+    q = get_threshold(spike_process)
+    x = 0.0
+    t = tmin
+    for i in axes(λ,1)
+        x += do_step(spike_process, λ[i], dt)
+        t += dt
+        if x >= q
+            push!(_sp, t)
+            q = get_threshold(spike_process)
+            x = 0.0
+        end
+    end
+    _sp
+end
+
+function generate_spikes(spike_process, λ::Matrix{Float64};kvs...)
+    ntrials = size(λ,2)
+    spikes = Vector{Vector{Float64}}(undef, ntrials)
+    for i in 1:ntrials
+        spikes[i] = generate_spikes(spike_process, λ[:,i];kvs...)
+    end
+    spikes
+end
+
 end
